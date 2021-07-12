cf507653b3d9ffeae54157d6fe8a7eb5e64f1c8d
Gboyega51 <gboyega51@gmail.com>
2021-07-12 01:03:10 +0100

    Reset buttin added

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,25 +1,28 @@
-import { useState } from "react";
-const Counter = () => {
-  const [count, setCount] = useState(0);
-
-  const handleIncrement = () => {
-    setCount(count + 1);
-  };
-
+const Counter = ({ onDelete, counter, onIncrement }) => {
   const getBadgeClasses = () => {
     let classes = " m-2 badge bg-";
-    classes += count === 0 ? "warning" : "primary";
+    classes += counter.value === 0 ? "warning" : "primary";
     return classes;
   };
 
   const formatCount = () => {
-    return count === 0 ? "Zero" : count;
+    return counter.value === 0 ? "Zero" : counter.value;
   };
+
   return (
     <div>
       <span className={getBadgeClasses()}>{formatCount()}</span>
-      <button onClick={handleIncrement} className="btn btn-secondary btn-sm">
+      <button
+        onClick={() => onIncrement(counter)}
+        className="btn btn-secondary btn-sm"
+      >
         Increment
+      </button>
+      <button
+        onClick={() => onDelete(counter.id)}
+        className="btn btn-danger btn-sm m-2"
+      >
+        Delete
       </button>
     </div>
   );
